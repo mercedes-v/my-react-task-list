b0313fc74acdf7586b1f7ce25dfae86f7f74a45d
mercedes  valencia <126997187+mercedes-v@users.noreply.github.com>
2023-05-29 20:52:45 -0500

    Delete my-react-task-list directory

--- my-react-task-list/componentes/header.jsx
+++
@@ -1,9 +0,0 @@
-import "./Header.css"
-
-export const Header = () => {
-    return (
-        <header className="head-container">
-            <h1 className="head-title">Todo app</h1>
-        </header>
-    );
-}
